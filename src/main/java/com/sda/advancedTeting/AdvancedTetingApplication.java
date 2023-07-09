ebdd104103ec2050491c753c88909b90f3074e63
Lauriantudor <52708705+Lauriantudor@users.noreply.github.com>
2023-07-09 09:31:14 +0300

    initial commit

---
+++ src/main/java/com/sda/advancedTeting/AdvancedTetingApplication.java
@@ -0,0 +1,13 @@
+package com.sda.advancedTeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedTetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedTetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/advancedTeting/AdvancedTetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.advancedTeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedTetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
