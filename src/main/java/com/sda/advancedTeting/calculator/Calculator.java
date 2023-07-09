38b09ddba62bd2e5fe9064cd42bc1068635044c6
Lauriantudor <52708705+Lauriantudor@users.noreply.github.com>
2023-07-09 10:40:18 +0300

    Calculator class has been created

---
+++ src/main/java/com/sda/advancedTeting/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package com.sda.advancedTeting.calculator;
+
+import com.sda.advancedTeting.calculator.exceptions.TruncatedResultException;
+
+public class Calculator {
+
+    public double add(double a, double b){
+        return  a+b;
+    }
+    public double subtract(double a, double b){
+        return  a-b;
+    }
+    public double multiply(double a, double b){
+        if (a> Double.MAX_VALUE){
+            throw new TruncatedResultException("Cannot multiply numbers of this size. Result will be truncated");
+        }
+        return  a*b;
+    }
+    public double divide(double a, double b){
+        if (b==0){
+            throw new IllegalArgumentException("Division by 0 is not supported");
+        }
+        return  a/b;
+    }
+    public int modulo(int a, int b){
+        if (b==0){
+            throw new IllegalArgumentException("Modulo by 0 is not supported");
+        }
+        return a%b;
+    }
+}
---
+++ src/main/java/com/sda/advancedTeting/calculator/exceptions/TruncatedResultException.java
@@ -0,0 +1,8 @@
+package com.sda.advancedTeting.calculator.exceptions;
+
+public class TruncatedResultException extends RuntimeException{
+
+    public TruncatedResultException(String message) {
+        super(message);
+    }
+}
